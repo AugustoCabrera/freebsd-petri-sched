22276c51f094ce9f31e20b55a9da7d8bed0a664a
AugustoCabrera <augustogabrielcabrera@mi.unc.edu.ar>
2026-01-02 21:36:00 -0300

    feat(reserved): modify resource net 4

--- code/releng14.1/sys/sys/sched_petri.h
+++ code/releng14.1/sys/sys/sched_petri.h
@@ -121,7 +121,8 @@ void wakeup_if_needed(struct thread *td);
 //Petri Global Methods
 int  resource_choose_cpu(struct thread *td);
 bool cpu_available_for_proc(int proc_id, int cpu);
-bool is_cpu_suspended(int cpu_n);
+bool is_cpu_disabled(int cpu_n);
+bool is_cpu_reserved(int cpu_n);
 void get_monopolized_cpus(int *dst);
 void free_double_pointer(void** pointer, int rows); 
 bool transition_is_sensitized(int transition_index);
